54b8be48d4498ab996d370ebd54893d9e4dfe7c7
Surya Durga Sai Sandeep Kadiyam <66644817+sandeepkadiyam@users.noreply.github.com>
2021-07-07 11:37:40 +0200

    Add src/quiz files via upload.

---
+++ src/quiz/cluster/kdtree.h
@@ -0,0 +1,137 @@
+/* \author Aaron Brown */
+// Quiz on implementing kd tree
+
+#include "../../render/render.h"
+
+
+// Structure to represent node of kd tree
+struct Node
+{
+	std::vector<float> point;
+	int id;
+	Node* left;
+	Node* right;
+
+	Node(std::vector<float> arr, int setId)
+	:	point(arr), id(setId), left(NULL), right(NULL)
+	{}
+};
+
+struct KdTree
+{
+	Node* root;
+
+	KdTree()
+	: root(NULL)
+	{}
+
+	void insertHelper (Node** node, int depth, std::vector<float> point, int id) {
+
+		if(*node == NULL) {
+			*node = new Node(point, id);
+		} else {
+			int cd = depth % 2;
+
+			if(point[cd] < ((*node)->point[cd])) {
+				insertHelper(&((*node)->left), depth+1, point, id);
+			} else {
+				insertHelper(&((*node)->right), depth+1, point, id);
+			}
+		}
+	}
+
+	void insert(std::vector<float> point, int id)
+	{
+		// TODO: Fill in this function to insert a new point into the tree
+		// the function should create a new node and place correctly with in the root 
+		insertHelper(&root, 0, point, id);
+
+	}
+
+	void searchHelper(Node* node, int depth, std::vector<float> target, float distanceTol, std::vector<int> &ids){
+
+		if(node != NULL) {
+
+			if((((node->point[0]) >= ( target[0] - distanceTol)) && ((node->point[0]) <= ( target[0] + distanceTol))) && (((node->point[1]) >= ( target[1] - distanceTol)) && ((node->point[1]) <= ( target[1] + distanceTol)))){
+				float distance = sqrt((((node->point[0]) - target[0]) * ((node->point[0]) - target[0])) + (((node->point[1]) - target[1]) * ((node->point[1]) - target[1])));
+				if(distance <= distanceTol) {
+					ids.push_back(node->id);
+				}
+			} 
+
+			if((target[depth%2] - distanceTol) < (node->point[depth%2])) {
+				searchHelper(node->left, depth+1, target, distanceTol, ids);
+			}
+
+			if((target[depth%2] + distanceTol) > (node->point[depth%2])) {
+				searchHelper(node->right, depth+1, target, distanceTol, ids);
+			}
+		}
+	} 
+
+	// return a list of point ids in the tree that are within distance of target
+	std::vector<int> search(std::vector<float> target, float distanceTol)
+	{
+		std::vector<int> ids;
+		searchHelper(root, 0, target, distanceTol, ids);
+		return ids;
+	}
+
+	void insertHelper3D (Node** node, int depth, std::vector<float> point, int id) {
+
+		if(*node == NULL) {
+			*node = new Node(point, id);
+		} else {
+			int cd = depth % 3;
+
+			if(point[cd] < ((*node)->point[cd])) {
+				insertHelper(&((*node)->left), depth+1, point, id);
+			} else {
+				insertHelper(&((*node)->right), depth+1, point, id);
+			}
+		}
+	}
+
+	void insert3D(std::vector<float> point, int id)
+	{
+		// TODO: Fill in this function to insert a new point into the tree
+		// the function should create a new node and place correctly with in the root 
+		insertHelper(&root, 0, point, id);
+
+	}
+
+	void searchHelper3D(Node* node, int depth, std::vector<float> target, float distanceTol, std::vector<int> &ids){
+
+		if(node != NULL) {
+
+			if((((node->point[0]) >= ( target[0] - distanceTol)) && ((node->point[0]) <= ( target[0] + distanceTol))) && (((node->point[1]) >= ( target[1] - distanceTol)) && ((node->point[1]) <= ( target[1] + distanceTol))) && (((node->point[2]) >= ( target[2] - distanceTol)) && ((node->point[2]) <= ( target[2] + distanceTol)))){
+				float distance = sqrt((((node->point[0]) - target[0]) * ((node->point[0]) - target[0])) + (((node->point[1]) - target[1]) * ((node->point[1]) - target[1])) + (((node->point[2]) - target[2]) * ((node->point[2]) - target[2])));
+				if(distance <= distanceTol) {
+					ids.push_back(node->id);
+				}
+			} 
+
+			if((target[depth%3] - distanceTol) < (node->point[depth%3])) {
+				searchHelper(node->left, depth+1, target, distanceTol, ids);
+			}
+
+			if((target[depth%3] + distanceTol) > (node->point[depth%3])) {
+				searchHelper(node->right, depth+1, target, distanceTol, ids);
+			}
+		}
+	} 
+
+	// return a list of point ids in the tree that are within distance of target
+	std::vector<int> search3D(std::vector<float> target, float distanceTol)
+	{
+		std::vector<int> ids;
+		searchHelper(root, 0, target, distanceTol, ids);
+		return ids;
+	}
+	
+
+};
+
+
+
+
---
+++ src/quiz/ransac/ransac2d.cpp
@@ -0,0 +1,254 @@
+/* \author Aaron Brown */
+// Quiz on implementing simple RANSAC line fitting
+
+#include "../../render/render.h"
+#include <unordered_set>
+#include "../../processPointClouds.h"
+// using templates for processPointClouds so also include .cpp to help linker
+#include "../../processPointClouds.cpp"
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData()
+{
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
+  	// Add inliers
+  	float scatter = 0.6;
+  	for(int i = -5; i < 5; i++)
+  	{
+  		double rx = 2*(((double) rand() / (RAND_MAX))-0.5);
+  		double ry = 2*(((double) rand() / (RAND_MAX))-0.5);
+  		pcl::PointXYZ point;
+  		point.x = i+scatter*rx;
+  		point.y = i+scatter*ry;
+  		point.z = 0;
+
+  		cloud->points.push_back(point);
+  	}
+  	// Add outliers
+  	int numOutliers = 10;
+  	while(numOutliers--)
+  	{
+  		double rx = 2*(((double) rand() / (RAND_MAX))-0.5);
+  		double ry = 2*(((double) rand() / (RAND_MAX))-0.5);
+  		pcl::PointXYZ point;
+  		point.x = 5*rx;
+  		point.y = 5*ry;
+  		point.z = 0;
+
+  		cloud->points.push_back(point);
+
+  	}
+  	cloud->width = cloud->points.size();
+  	cloud->height = 1;
+
+  	return cloud;
+
+}
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData3D()
+{
+	ProcessPointClouds<pcl::PointXYZ> pointProcessor;
+	return pointProcessor.loadPcd("../../../sensors/data/pcd/simpleHighway.pcd");
+}
+
+
+pcl::visualization::PCLVisualizer::Ptr initScene()
+{
+	pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer ("2D Viewer"));
+	viewer->setBackgroundColor (0, 0, 0);
+  	viewer->initCameraParameters();
+  	viewer->setCameraPosition(0, 0, 15, 0, 1, 0);
+  	viewer->addCoordinateSystem (1.0);
+  	return viewer;
+}
+
+std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+{
+	std::unordered_set<int> inliersResult;
+	srand(time(NULL));
+	
+	// TODO: Fill in this function
+
+	// For max iterations 
+	for (int i = 0; i < maxIterations; ++i) {
+
+		std::unordered_set <int> inliers;
+
+		while(inliers.size() < 2) {
+			inliers.insert(rand() % (cloud->points.size()));
+		}
+
+		/*int random_number1 = rand() % (cloud->points.size());
+		do
+		{
+			int random_number2 = rand() % (clouds->points.size());
+		} while (random_number1 == random_number2);*/
+
+		auto itr = inliers.begin();
+
+		float x1 = cloud->points[*itr].x;
+		float y1 = cloud->points[*itr].y;
+		itr++;
+		float x2 = cloud->points[*itr].x;
+		float y2 = cloud->points[*itr].y;
+
+		float a = (y1 - y2);
+		float b = (x2 - x1);
+		float c = ((x1 * y2) - (x2 * y1));
+
+		for (int j = 0; j < (cloud->points.size()); ++j) {
+
+			if (inliers.count(j) > 0){
+				continue;
+			}
+			
+			float x = (cloud->points[j].x);
+			float y = (cloud->points[j].y);
+
+			float numerator = ((a * x) + (b * y) + c);
+			float denominator = sqrt((a * a) + (b * b));
+
+			float distance = fabs(numerator / denominator);
+
+
+			if (distance < distanceTol) {
+				inliers.insert(j);
+			}
+		}
+
+		if (inliers.size() > inliersResult.size()) {
+			inliersResult = inliers;
+		}
+	}
+
+	// Randomly sample subset and fit line
+
+	// Measure distance between every point and fitted line
+	// If distance is smaller than threshold count it as inlier
+
+	// Return indicies of inliers from fitted line with most inliers
+	
+	return inliersResult;
+
+}
+
+std::unordered_set<int> RansacPlane(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+{
+	std::unordered_set<int> inliersResult;
+	srand(time(NULL));
+	
+	// TODO: Fill in this function
+
+	// For max iterations 
+	for (int i = 0; i < maxIterations; ++i) {
+
+		std::unordered_set <int> inliers;
+
+		while(inliers.size() < 3) {
+			inliers.insert(rand() % (cloud->points.size()));
+		}
+
+		/*int random_number1 = rand() % (cloud->points.size());
+		do
+		{
+			int random_number2 = rand() % (clouds->points.size());
+		} while (random_number1 == random_number2);*/
+
+		auto itr = inliers.begin();
+
+		float x1 = cloud->points[*itr].x;
+		float y1 = cloud->points[*itr].y;
+		float z1 = cloud->points[*itr].z;
+		itr++;
+		float x2 = cloud->points[*itr].x;
+		float y2 = cloud->points[*itr].y;
+		float z2 = cloud->points[*itr].z;
+		itr++;
+		float x3 = cloud->points[*itr].x;
+		float y3 = cloud->points[*itr].y;
+		float z3 = cloud->points[*itr].z;
+
+		float a = ((y2 - y1) * (z3 - z1)) - ((z2 - z1) * (y3 - y1));
+		float b = ((z2 - z1) * (x3 - x1)) - ((x2 - x1) * (z3 - z1));
+		float c = ((x2 - x1) * (y3 - y1)) - ((y2 - y1) * (x3 - x1));
+		float d = -((a * x1) + (b * y1) + (c * z1));
+
+		for (int j = 0; j < (cloud->points.size()); ++j) {
+
+			if (inliers.count(j) > 0){
+				continue;
+			}
+			
+			float x = (cloud->points[j].x);
+			float y = (cloud->points[j].y);
+			float z = (cloud->points[j].z);
+
+			/*float numerator = ((a * x) + (b * y) + (c * z) + d);
+			float denominator = sqrt((a * a) + (b * b) + (c * c));*/
+
+			float distance = fabs((a * x) + (b * y) + (c * z) + d)/ sqrt((a * a) + (b * b) + (c * c));
+
+
+			if (distance < distanceTol) {
+				inliers.insert(j);
+			}
+		}
+
+		if (inliers.size() > inliersResult.size()) {
+			inliersResult = inliers;
+		}
+	}
+
+	// Randomly sample subset and fit line
+
+	// Measure distance between every point and fitted line
+	// If distance is smaller than threshold count it as inlier
+
+	// Return indicies of inliers from fitted line with most inliers
+	
+	return inliersResult;
+
+}
+
+int main ()
+{
+
+	// Create viewer
+	pcl::visualization::PCLVisualizer::Ptr viewer = initScene();
+
+	// Create data
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData3D();
+	
+
+	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
+	std::unordered_set<int> inliers = RansacPlane(cloud, 30, 1.0);
+
+	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
+
+	for(int index = 0; index < cloud->points.size(); index++)
+	{
+		pcl::PointXYZ point = cloud->points[index];
+		if(inliers.count(index))
+			cloudInliers->points.push_back(point);
+		else
+			cloudOutliers->points.push_back(point);
+	}
+
+
+	// Render 2D point cloud with inliers and outliers
+	if(inliers.size())
+	{
+		renderPointCloud(viewer,cloudInliers,"inliers",Color(0,1,0));
+  		renderPointCloud(viewer,cloudOutliers,"outliers",Color(1,0,0));
+	}
+  	else
+  	{
+  		renderPointCloud(viewer,cloud,"data");
+  	}
+	
+  	while (!viewer->wasStopped ())
+  	{
+  	  viewer->spinOnce ();
+  	}
+  	
+}
